12e896f9fdf344fa9e72cd7540c18307a2e53fa6
funnker <67109377+funnker@users.noreply.github.com>
2021-04-28 14:58:52 +0300

    ux update

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ const newTaskForm = document.querySelector('[data-new-task-form]')
 const newTaskInput = document.querySelector('[data-new-task-input')
 const clearCompleteTasksButton = document.querySelector('[data-clear-complete-tasks-button]')
 
-
 const LOCAL_STORAGE_LIST_KEY = 'task.lists'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
